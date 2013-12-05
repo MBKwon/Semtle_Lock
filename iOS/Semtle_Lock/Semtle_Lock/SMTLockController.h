c8f03a18e6837262dfe64f94f589078cd6350f66
Moonbeom Kyle KWON <maskkwon@gmail.com>
2013-12-05 11:12:38 +0900

    make methods for saving, matched.

--- iOS/Semtle_Lock/Semtle_Lock/SMTLockController.h
+++ iOS/Semtle_Lock/Semtle_Lock/SMTLockController.h
@@ -13,12 +13,16 @@
 #define SMT_LOCK_NOT_KEY                        0x00000001
 #define SMT_LOCK_ENCRYPTION_FAIL                0x00000002
 #define SMT_LOCK_FILESAVE_FAIL                  0x00000003
+#define SMT_LOCK_FILELOAD_FAIL                  0x00000004
+#define SMT_LOCK_PASS_NOT_MATCH                 0x00000005
 
 
 @interface SMTLockController : NSObject
 
 @property (nonatomic, strong) NSString *key;
 
--(NSInteger)setPassword:(NSString *)password;
+
+-(SMT_RESULT_CODE)setPassword:(NSString *)password;
+-(SMT_RESULT_CODE)matchPassword:(NSString *)password;
 
 @end
--- iOS/Semtle_Lock/Semtle_Lock/SMTLockController.m
+++ iOS/Semtle_Lock/Semtle_Lock/SMTLockController.m
@@ -11,8 +11,43 @@
 
 @implementation SMTLockController
 
+-(SMT_RESULT_CODE)savePass:(NSData *)encryptedData
+{
+    
+    NSString *fullPath = [DEFAULT_PATH  stringByAppendingString:@"/"];
+    fullPath = [fullPath stringByAppendingString:FILE_NAME];
+    
+    BOOL result = [encryptedData writeToFile:FILE_NAME atomically:YES];
+    
+    if (result == YES) {
+        return SMT_LOCK_OK;
+    } else {
+        return SMT_LOCK_FILESAVE_FAIL;
+    }
+}
 
--(NSInteger)setPassword:(NSString *)password
+-(SMT_RESULT_CODE)loadPass:(NSData *)encryptedData
+{
+    
+    NSString *fullPath = [DEFAULT_PATH  stringByAppendingString:@"/"];
+    fullPath = [fullPath stringByAppendingString:FILE_NAME];
+    
+    
+    BOOL result = [[NSFileManager defaultManager] fileExistsAtPath:fullPath];
+    if (!result) {
+        
+        return SMT_LOCK_FILELOAD_FAIL;
+        
+    } else {
+        
+        encryptedData = [NSData dataWithContentsOfFile:fullPath];
+        return SMT_LOCK_OK;
+    }
+}
+
+
+
+-(SMT_RESULT_CODE)setPassword:(NSString *)password
 {
     if (_key == nil) {
         
@@ -26,18 +61,50 @@
         if (encryptedData == nil) {
             
             return SMT_LOCK_ENCRYPTION_FAIL;
-        
+            
         } else {
             
-            BOOL result = [encryptedData writeToFile:@"SMT_ITEM_PASS" atomically:YES];
+            SMT_RESULT_CODE result = [self savePass:encryptedData];
             
-            if (result == YES) {
-                return SMT_LOCK_OK;
+            return result;
+        }
+    }
+}
+
+-(SMT_RESULT_CODE)matchPassword:(NSString *)password
+{
+    if (_key == nil) {
+        
+        return SMT_LOCK_NOT_KEY;
+        
+    } else {
+        
+        NSData *rowData = [password dataUsingEncoding:NSUTF8StringEncoding];
+        NSData *encryptedData = [rowData encryptAES256WithKey:_key];
+        
+        if (encryptedData == nil) {
+            
+            return SMT_LOCK_ENCRYPTION_FAIL;
+            
+        } else {
+            
+            NSData *savedPassData = [NSData new];
+            SMT_RESULT_CODE result = [self loadPass:savedPassData];
+            
+            if (result == SMT_LOCK_OK) {
+                
+                if ([encryptedData isEqualToData:savedPassData] == YES) {
+                    return result;
+                } else {
+                    return SMT_LOCK_PASS_NOT_MATCH;
+                }
+                
             } else {
-                return SMT_LOCK_FILESAVE_FAIL;
+                return result;
             }
         }
     }
 }
+
 
 @end
