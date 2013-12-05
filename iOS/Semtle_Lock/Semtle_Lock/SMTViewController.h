2dc7c756b9bb09c7dd573bc5116808578c380d60
Moonbeom Kyle KWON <maskkwon@gmail.com>
2013-12-05 10:08:47 +0900

    make iOS project

---
+++ iOS/Semtle_Lock/Semtle_Lock/SMTViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMTViewController.h
+//  Semtle_Lock
+//
+//  Created by MB KWON on 2013. 12. 5..
+//  Copyright (c) 2013년 Semtle_men. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMTViewController : UIViewController
+
+@end
---
+++ iOS/Semtle_Lock/Semtle_Lock/SMTViewController.m
@@ -0,0 +1,29 @@
+//
+//  SMTViewController.m
+//  Semtle_Lock
+//
+//  Created by MB KWON on 2013. 12. 5..
+//  Copyright (c) 2013년 Semtle_men. All rights reserved.
+//
+
+#import "SMTViewController.h"
+
+@interface SMTViewController ()
+
+@end
+
+@implementation SMTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
